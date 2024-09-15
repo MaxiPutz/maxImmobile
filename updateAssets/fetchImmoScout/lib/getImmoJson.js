19cfe5efa9c6dc89fc2da98d68eb8d6dd1c4918d
max <max>
2024-09-15 16:33:15 +0200

    delete logs

--- updateAssets/fetchImmoScout/lib/getImmoJson.js
+++ updateAssets/fetchImmoScout/lib/getImmoJson.js
@@ -7,25 +7,20 @@ export const getImmoJson =  (url) => {
         let script = document.querySelectorAll("script");
         script = [...script].map(ele => ele.text).filter(ele => ele.includes("window.__INITIAL_STATE__="));
 
-        console.log(script);
 
         let data = script[0].split("window.__INITIAL_STATE__=")[1];
         data = data.split("window")[0].trim();  // Adjusted split
 
-        console.log('Data before parsing:', data);
 
         data = data.replace(/undefined/g, 'null'); // Replace all 'undefined' with 'null'
 
         try {
             data = JSON.parse(data);
-            console.log(data);
 
             const results = data.reduxAsyncConnect.pageData.results.hits
-            console.log("results", results)
 
 
             let tmp = results.filter(ele => ele.headline.toLocaleLowerCase().includes("balkon"))
-            console.log(tmp)
 
             return results
             // addressString
