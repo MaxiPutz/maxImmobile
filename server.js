c7d83bd78ab03312ac5e5d53aca86e51cff9f846
max <max>
2024-10-29 10:29:05 +0100

    add new asssets

--- server.js
+++ server.js
@@ -15,18 +15,33 @@ app.use(express.static('public'));
 
 app.listen(port, () => console.log("server is listen on port " + port + "\n" + `productiv is if you run npm build then you can join the webside on http://yourUrl:${port}` + ` debug version is on http://localhost:${port}/new`))
 
-const transitPath = "./static/assets/transit/"
-const files = fs.readdirSync(transitPath).filter(ele => ele.includes(".json"))
-console.log(files);
- 
-
+const transitPath = "./assets/transit/"
+let files = fs.readdirSync(transitPath).filter(ele => ele.includes(".json"))
 
 fs.writeFileSync(transitPath + "index.js", `
   export const transitOptions = ${JSON.stringify(files)}
   `)
 
+files = fs.readdirSync(transitPath)
+
+const staticDirs = [
+  "./public/assets/transit/",
+  "./staticNewUI/assets/transit/",
+  "./static/assets/transit/" 
+]
+
+staticDirs.flatMap(dir => fs.readdirSync(dir).map(_dir => dir+ _dir).filter(e => e.includes(".js")) ).forEach(ele => fs.unlinkSync(ele))
+
+files.forEach(file => {
+  const content = fs.readFileSync(transitPath+"/" + file).toString()
+
+  staticDirs.forEach(dir => fs.writeFileSync(dir + file, content))
+})
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 })
+
+
 
 
