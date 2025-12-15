92490d59500f730baf71774df7ec3a1397f6dd72
Maximilian Putz <max@Mac.home>
2025-12-15 22:18:20 +0100

    old commit

--- updateWillhabenInput.sh
+++ updateWillhabenInput.sh
@@ -24,6 +24,18 @@ node index.js
 cd "../../"
 
 cd "./updateAssets/mergeInput/"
+FILE_WH="../fetchWillhabenImmo/result.json"
+
+if [ ! -f "$FILE_WH" ]; then
+  echo "[]" > "$FILE_WH"
+fi
+node index.js
+## run the nextline to merge willhaben and immoscout
+FILE_IS="../fetchImmoScout/result.json"
+
+if [ ! -f "$FILE_IS" ]; then
+  echo "[]" > "$FILE_IS"
+fi
 
 
 ## run the nextline to merge willhaben and immoscout
@@ -40,7 +52,7 @@ europeanDate=$(date +'%d-%m-%Y')
 zip -j backup/willHabenimmo_${europeanDate}.zip "./updateAssets/fetchWillhabenImmo/result.json"
 zip -j backup/immoScoummp_${europeanDate}.zip "./updateAssets/fetchImmoScout/result.json"
 
-cp ./updateAssets/fetchImmoScout/result.json ./public/assets/input.json
+# cp ./updateAssets/fetchImmoScout/result.json ./public/assets/input.json
 
 # Print success message
 echo "Update complete and backups created: willHabenimmo_${europeanDate}.zip and source2immo_${europeanDate}.zip"
